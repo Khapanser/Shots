76baa9384c54e52c1f650aef507ad1a499d697d7
khapanser <khapanser@gmail.com>
2018-12-28 16:07:13 +0300

    colour refact

--- src/main/java/nameController.java
+++ src/main/java/nameController.java
@@ -1,35 +1,55 @@
 package main.java;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class nameController {
+public class NameController {
 
-        public void showDialog(ActionEvent actionEvent){
+    public static String nickname;
+    @FXML
+    public TextArea nameField;
+    private FXMLLoader loader;
+    private Stage util;
 
-            try {
-                Stage stage = new Stage();
-                Parent name = FXMLLoader.load(getClass().getResource("../../fxml/name.fxml"));
-                stage.setTitle("Name");
-                stage.setMinHeight(150);
-                stage.setMinWidth(300);
-                stage.setResizable(false);
-                stage.setScene(new Scene(name));
-                stage.initModality(Modality.APPLICATION_MODAL);
-                stage.initOwner(((Node)actionEvent.getSource()).getScene().getWindow());
-                stage.show();
-            }catch(IOException e){
-                System.out.println("не запустилось окно NAME");
-            }
+    public NameController(FXMLLoader loader){
+        this.loader = loader;
+        this.showDialog();
+    }
 
+    public NameController(){
+
+    }
+
+
+    public void showDialog() {
+
+        try {
+            Stage util = new Stage(StageStyle.TRANSPARENT);
+            util.initModality(Modality.APPLICATION_MODAL);
+            Parent name = FXMLLoader.load(getClass().getResource("../../fxml/name.fxml"));
+            util.setScene(new Scene(name));
+
+            util.show();
+        } catch (IOException e) {
+            System.out.println("не запустилось окно NAME");
         }
 
+    }
 
+    //Через getController передаём Controller введённый никнейм.
+    public void nameButton(ActionEvent actionEvent) {
+      /*  this.nickname = (String) nameField.getText();
+        Controller controller = loader.getController();
+        controller.messFromNameCtrl(nickname);
+        util.close();*/
+    }
 }
